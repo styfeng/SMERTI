64b5f7d2e5290734a1800270aac77e756e2f7adc
Steven Feng <stevenyfeng@gmail.com>
2020-01-05 04:01:31 -0500

    Refactored code for data preprocessing and updated README

---
+++ Data Preprocessing and Preparation/Dataset_preprocessing.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+import io, json, os, collections, pprint, time
+import re
+from string import punctuation
+import unicodedata
+import random
+
+
+def isEnglish(s):
+    try:
+        s.encode(encoding='utf-8').decode('ascii')
+    except UnicodeDecodeError:
+        return False
+    else:
+        return True
+
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def process_text(s):
+    s = unicodeToAscii(s.lower().strip()) 
+    s = re.sub('\!+', '!', s)
+    s = re.sub('\,+', ',', s)
+    s = re.sub('\?+', '?', s)
+    s = re.sub('\.+', '.', s)    
+    s = re.sub("[^a-zA-Z.!?,'']+", ' ', s)
+    for p in punctuation:
+        if p not in ["'", "[", "]"]:
+            s = s.replace(p, " " + p + " ")       
+    s = re.sub(' +', ' ', s)
+    s = s.strip()
+    return s
+
+
+def filter_headlines(path, max_length):
+    headline_list = []
+    f = io.open(path, encoding = 'utf-8')
+    counter = 0
+    print("Currently reading lines from file ...")
+    for l in f:
+        if counter % 10000 == 0:
+            print("Read in {%d} lines" % counter)
+        jline = json.loads(l)
+        if len(jline['headline'].split()) <= max_length and jline['headline'] != '' and isEnglish(jline['headline']) \
+           and 'http' not in jline['headline'].lower() and 'www' not in jline['headline'].lower():
+            clean_line = re.sub('\s+', ' ', jline['headline']).strip()
+            clean_line_final = process_text(clean_line)
+            headline_list.append(clean_line_final + '\n')
+        counter += 1
+    return headline_list
+
+
+def filter_reviews(path, max_length, dataset):
+    review_list = []
+    positive_list = []
+    negative_list = []
+    neutral_list = []
+    f = io.open(path, encoding = 'utf-8')
+    counter = 0
+    print("Currently reading lines from file ...")
+    
+    if dataset.lower() == 'yelp':
+        for l in f:
+            if counter % 100000 == 0:
+                print("Read in {%d} lines" % counter)
+            jline = json.loads(l)
+            if len(jline['text'].split()) <= max_length and jline['text'] != '' and isEnglish(jline['text']) \
+               and 'http' not in jline['text'].lower() and 'www' not in jline['text'].lower():
+                clean_line = re.sub('\s+', ' ', jline['text']).strip()
+                clean_line_final = process_text(clean_line)
+                if jline['stars'] < 3.0:
+                    negative_list.append(clean_line_final + '\n')
+                elif jline['stars'] == 3.0:
+                    neutral_list.append(clean_line_final + '\n')
+                else:
+                    positive_list.append(clean_line_final + '\n')
+                review_list.append(clean_line_final + '\n')
+            counter += 1
+    
+    elif dataset.lower() == 'amazon':
+        for l in f:
+            if counter % 100000 == 0:
+                print("Read in {%d} lines" % counter)
+            jline = json.loads(l)
+            if len(jline['reviewText'].split()) <= max_length and jline['reviewText'] != '' and isEnglish(jline['reviewText']) and 'http' not in jline['reviewText'].lower() and 'www' not in jline['reviewText'].lower():
+                clean_line = re.sub('\s+', ' ', jline['reviewText']).strip()
+                clean_line_final = process_text(clean_line)
+                if jline['overall'] < 3.0:
+                    negative_list.append(clean_line_final + '\n')
+                elif jline['overall'] == 3.0:
+                    neutral_list.append(clean_line_final + '\n')
+                else:
+                    positive_list.append(clean_line_final + '\n')
+                review_list.append(clean_line_final + '\n')
+            counter += 1        
+    
+    return review_list, positive_list, negative_list, neutral_list
+
+
+def write_file(lst, path):
+    f = io.open(path, "w", encoding = 'utf-8')
+    print("Currently writing lines to file ...")
+    f.writelines(lst)
+    f.close()
+    print("Lines successfully written to file!")
+
+
+#For news headlines:
+
+path1 = os.path.expanduser('News_Dataset/News_Category_Dataset_v2.json')
+path2 = os.path.expanduser('News_Dataset/transformer_data/train_headlines.txt')
+path3 = os.path.expanduser('News_Dataset/transformer_data/val_headlines.txt')
+path4 = os.path.expanduser('News_Dataset/transformer_data/test_headlines.txt')
+
+max_length = 20
+
+headline_list = filter_headlines(path1, max_length)
+total_headlines = len(headline_list)
+print("Total number of headlines: ", total_headlines)
+
+random.shuffle(headline_list)
+
+train_list = headline_list[:120000]
+val_list = headline_list[120000:140000]
+test_list = headline_list[140000:160000]
+
+#write_file(train_list, path2)
+#write_file(val_list, path3)
+#write_file(test_list, path4)
+
+
+#For reviews:
+
+path1 = os.path.expanduser('Yelp_Dataset/review.json')
+
+path2 = os.path.expanduser('Yelp_Dataset/transformer_data/train_reviews_positive.txt')
+path3 = os.path.expanduser('Yelp_Dataset/transformer_data/train_reviews_negative.txt')
+path4 = os.path.expanduser('Yelp_Dataset/transformer_data/train_reviews_neutral.txt')
+
+path5 = os.path.expanduser('Yelp_Dataset/transformer_data/valid_reviews_positive.txt')
+path6 = os.path.expanduser('Yelp_Dataset/transformer_data/valid_reviews_negative.txt')
+path7 = os.path.expanduser('Yelp_Dataset/transformer_data/valid_reviews_neutral.txt')
+
+path8 = os.path.expanduser('Yelp_Dataset/transformer_data/test_reviews_positive.txt')
+path9 = os.path.expanduser('Yelp_Dataset/transformer_data/test_reviews_negative.txt')
+path10 = os.path.expanduser('Yelp_Dataset/transformer_data/test_reviews_neutral.txt')
+
+path11 = os.path.expanduser('Yelp_Dataset/transformer_data/final_train_reviews.txt')
+path12 = os.path.expanduser('Yelp_Dataset/transformer_data/final_valid_reviews.txt')
+path13 = os.path.expanduser('Yelp_Dataset/transformer_data/final_test_reviews.txt')
+
+
+max_length = 20
+
+review_list, positive_list, negative_list, neutral_list = filter_reviews(path1, max_length, 'yelp')
+
+total_reviews = len(review_list)
+print("Number of total reviews: ", total_reviews)
+
+positive_reviews = len(positive_list)
+print("Number of positive reviews: ", positive_reviews)
+
+negative_reviews = len(negative_list)
+print("Number of negative reviews: ", negative_reviews)
+
+neutral_reviews = len(neutral_list)
+print("Number of neutral reviews: ", neutral_reviews)
+
+random.shuffle(positive_list)
+random.shuffle(negative_list)
+random.shuffle(neutral_list)
+
+train_positive = positive_list[:30000]
+train_negative = negative_list[:30000]
+train_neutral = neutral_list[:15000]
+
+valid_positive = positive_list[30000:35000]
+valid_negative = negative_list[30000:35000]
+valid_neutral = neutral_list[15000:17500]
+
+test_positive = positive_list[35000:40000]
+test_negative = negative_list[35000:40000]
+test_neutral = neutral_list[17500:20000]
+
+final_train_reviews = train_positive + train_negative + train_neutral
+final_valid_reviews = valid_positive + valid_negative + valid_neutral
+final_test_reviews = test_positive + test_negative + test_neutral
+
+#write_file(train_positive, path2)
+#write_file(train_negative, path3)
+#write_file(train_neutral, path4)
+
+#write_file(valid_positive, path5)
+#write_file(valid_negative, path6)
+#write_file(valid_neutral, path7)
+
+#write_file(test_positive, path8)
+#write_file(test_negative, path9)
+#write_file(test_neutral, path10)
+
+#write_file(final_train_reviews, path11)
+#write_file(final_valid_reviews, path12)
+#write_file(final_test_reviews, path13)
--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Code for the SMERTI pipeline designed for Semantic Text Exchange, presented in [
 
 ## Following is a brief description of each file:
 
-1. Dataset_preprocessing_(reviews) and Dataset_preprocessing_(news) are for preprocessing the reviews (e.g. Yelp and Amazon) and news headlines, respectively, generating training and testing splits, and writing them to .txt files. (Data Preprocessing and Preparation)
-2. Dataset_masking_(reviews) and Dataset_masking_(news) are for generating masked versions of the training and testing data from step 1. for reviews and news headlines, respectively, and writing them to .txt files. (Data Preprocessing and Preparation)
+1. Dataset_preprocessing is for preprocessing the reviews (e.g. Yelp and Amazon) and news headlines, generating training and testing splits, and writing them to .txt files. (Data Preprocessing and Preparation)
+2. Dataset_masking is for generating masked versions of the training and testing data from step 1. for reviews and news headlines, and writing them to .txt files. (Data Preprocessing and Preparation)
 3. RNN_training and Transformer_training are to train the TIM for SMERTI-RNN and SMERTI-Transformer, respectively, based on the .txt files generated from steps 1. and 2. above. (Text Infilling Module (TIM) Training)
 4. RNN_final_pipeline and Transformer_final_pipeline are the final pipelines for SMERTI-RNN and SMERTI-Transformer, respectively. This includes the ERM, SMM, and TIM components of each pipeline variation. Also note that the bottom of each file includes a section to generate and write output to .txt files, based on the evaluation lines generated from step 5. below, to be used in metric calculations in step 6. (Final SMERTI Pipelines)
-5. Evaluation_setup is to generate the evaluation lines for the datasets and includes choosing nouns, choosing evaluation lines per noun, and writing them to .txt files. (Evaluation)
-6. Final_evaluation is to calculate metrics including SPA, BLEU, Perplexity, and SLOR using SMERTI's output from the evaluation lines, and includes functions to write results to .txt files. (Evaluation)
+5. Evaluation_setup is to generate the evaluation lines for the datasets and includes choosing nouns, choosing evaluation lines per noun, and writing them to .txt files. (Evaluation) (*Note: code for other parts-of-speech will be released later)
+6. Final_evaluation is to calculate metrics including SPA, BLEU, Perplexity, and SLOR using SMERTI's output from the evaluation lines, and includes functions to write results to .txt files. (Evaluation) (*Note: code for baselines and updated metrics (e.g. CSS and STES) will be released later)
 7. SLOR_normalization is to normalize all SLOR values (81,000 in our case) calculated from step 6. above to a [0,1] interval. (Evaluation)
 
 
